701e76a880df5745e8e748cd268c8cadb95d8fed
Max_Trepalin <74497851+MaxTrepalin@users.noreply.github.com>
2021-05-19 17:45:04 +0300

    Update MainWindow.xaml.cs

--- FuelSt/MainWindow.xaml.cs
+++ FuelSt/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +20,71 @@ namespace FuelSt
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        double allSumm = 0;
+        double qFuel = 0;
+        double fuelPrice=0;
+        double costFuel =0;
+
+
         public MainWindow()
         {
+           
             InitializeComponent();
         }
+
+        private void ComboBox_Fuel_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ComboBox_Fuel.Items.Add("92");
+            ComboBox_Fuel.Items.Add("95");
+            ComboBox_Fuel.Items.Add("100");
+            ComboBox_Fuel.Items.Add("DT");
+
+          
+        }
+
+        private void Button_AllSum_Click(object sender, RoutedEventArgs e)
+        {
+            qFuel = Convert.ToDouble (Liters.Text);
+            costFuel = qFuel * fuelPrice;
+            allSumm = costFuel;
+
+            if (Check_burger.IsChecked == true)
+            { allSumm += 200; }
+            if (Check_Tea.IsChecked == true)
+            { allSumm += 20; }
+            if (Check_cofe.IsChecked == true)
+            { allSumm += 50; }
+            if (Check_cola.IsChecked == true)
+            { allSumm += 60; }
+            if (Check_hotDog.IsChecked == true)
+            { allSumm += 130; }
+
+            TextBoxAllSumm.Text = Convert.ToString(allSumm) + " руб.";
+            FuelSumm.Text = Convert.ToString(costFuel);
+        }
+
+        private void ComboBox_Fuel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBox_Fuel.SelectedItem == "92")
+            {
+                fuelPrice = 43;
+            }
+            if (ComboBox_Fuel.SelectedItem == "95")
+            {
+                fuelPrice = 49;
+            }
+            if (ComboBox_Fuel.SelectedItem == "100")
+            {
+                fuelPrice = 51;
+            }
+            if (ComboBox_Fuel.SelectedItem == "DT")
+            {
+                fuelPrice = 50;
+            }
+
+        }
+
+       
     }
 }
